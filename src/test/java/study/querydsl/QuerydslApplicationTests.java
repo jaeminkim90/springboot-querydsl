fe5c597ce4e3969fbb5e14a99d5cc41f088ffcb5
Jaemin Kim <jaeminkim90@gmail.com>
2022-04-22 17:37:25 +0900

    test: QueryDSL을 사용하여 조회한 엔티티가 실제 엔티티와 일치하는지 확인

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	// @Autowired // 스프리스프링 최신 버전에서는 Autowired 사용 가능
+	@PersistenceContext // 다른 프레임워크 사용 가능성이 있다면 이걸 사용하자
+		EntityManager em;
 
 	@Test
 	void contextLoads() {
-	}
+		Hello hello = new Hello();
+		em.persist(hello); // 엔티티 영속성 컨텍스트 저장
 
+		JPAQueryFactory query = new JPAQueryFactory(em); // JPAQueryFactory 사용 권장
+		QHello qHello = new QHello("h");
+
+		// QueryDSL 사용 예시
+		// 쿼리와 관련된 것은 모두 Q타입을 사용한다.
+		Hello result = query
+			.selectFrom(qHello)
+			.fetchOne();
+
+		// 검증
+		Assertions.assertThat(result).isEqualTo(hello);
+	}
 }
